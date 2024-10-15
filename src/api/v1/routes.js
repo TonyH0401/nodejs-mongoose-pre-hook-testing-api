81a4e63b83010b7d4dd8e7be267987fb3bc0aca3
Lê Gia Phú <phugiale2912@gmail.com>
2024-10-15 09:12:58 +0700

    feat: init User

---
+++ src/api/v1/routes.js
@@ -0,0 +1,26 @@
+// --------------------------
+// Section: Package Requirements
+// --------------------------
+const router = require("express").Router();
+
+// --------------------------
+// Section: Custom Utils Requirements
+// --------------------------
+
+// --------------------------
+// Section: Custom Middlewares
+// --------------------------
+
+// --------------------------
+// Section: Connect to Databases
+// --------------------------
+
+// --------------------------
+// Section: Routers
+// --------------------------
+
+
+// --------------------------
+// Section: Exports
+// --------------------------
+module.exports = router;
