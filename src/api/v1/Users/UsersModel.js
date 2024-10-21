7603a987f25878ce9b953c20dc3adc8e88a19983
Lê Gia Phú <phugiale2912@gmail.com>
2024-10-21 18:45:20 +0700

    refactor: change users schema for pre hook

--- src/api/v1/Users/UsersModel.js
+++ src/api/v1/Users/UsersModel.js
@@ -2,6 +2,7 @@
 // Section: Package Requirements
 // --------------------------
 const Schema = require("mongoose").Schema;
+const createError = require("http-errors");
 
 // --------------------------
 // Section: Import Database Connection
@@ -9,46 +10,53 @@ const Schema = require("mongoose").Schema;
 const { mongodb_conn } = require("../../../database/mongoose");
 
 // --------------------------
-// Section: Define UsersModel
+// Section: Define UsersModel - UsersSchema
 // --------------------------
-const UsersModel = mongodb_conn.model(
-  "UsersModel",
-  new Schema(
-    {
-      userFullName: { type: String, required: [true, "{PATH} is required"] },
-      userEmail: {
-        type: String,
-        required: [true, "{PATH} is required"],
-        unique: true,
-      },
-      userAge: {
-        type: Number,
-        required: [true, "{PATH} is required"],
-      },
-      userGender: {
-        type: String,
-        enum: {
-          values: ["male", "female", "other"],
-          message: "{VALUE} is not supported",
-        },
-        default: "other",
-      },
-      militarySchoolName: {
-        type: String,
-        required: [
-          function () {
-            return this.userAge > 20;
-          },
-          "Citizen of Age by Laws must applied to a Military School!",
-        ],
-        default: null,
-      },
+const UsersSchema = new Schema(
+  {
+    userFullName: { type: String, required: [true, "{PATH} is required"] },
+    userEmail: {
+      type: String,
+      required: [true, "{PATH} is required"],
+      unique: true,
     },
-    { timestamps: true }
-  )
+    userAge: {
+      type: Number,
+      required: [true, "{PATH} is required"],
+    },
+    userGender: {
+      type: String,
+      enum: {
+        values: ["male", "female", "other"],
+        message: "{VALUE} is not supported",
+      },
+      default: "other",
+    },
+    militarySchoolName: {
+      type: String,
+      required: [
+        function () {
+          return this.userAge > 20;
+        },
+        "Citizen of Age by Laws must applied to a Military School!",
+      ],
+      default: null,
+    },
+  },
+  { timestamps: true }
 );
+
+// --------------------------
+// Section: Using '.pre()' Hooks
+// --------------------------
+// UsersModel.pre("save", async function (next) {
+//   try {
+//   } catch (error) {
+//     return next(createError(500, error.message));
+//   }
+// });
 
 // --------------------------
 // Section: Exports
 // --------------------------
-module.exports = UsersModel;
+module.exports = mongodb_conn.model("UsersModel", UsersSchema);
