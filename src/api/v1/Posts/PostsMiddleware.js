f5024355a5ddcc07e75574b94e7248b8ee9bb1e1
Lê Gia Phú <phugiale2912@gmail.com>
2024-10-22 21:15:24 +0700

    feat: getComment by content

--- src/api/v1/Posts/PostsMiddleware.js
+++ src/api/v1/Posts/PostsMiddleware.js
@@ -43,3 +43,21 @@ module.exports.createPost = async (req, res, next) => {
     return next(createError(500, error.message));
   }
 };
+
+module.exports.getComment = async (req, res, next) => {
+  const { commentParam } = req.params;
+  try {
+    const foundComment = await PostsModel.findOne({
+      "comments.commentContent": commentParam,
+    }).exec();
+    if (!foundComment) return next(createError(404, "No comment found!"));
+    return res.status(200).json({
+      code: 1,
+      success: true,
+      message: "Found comment!",
+      data: foundComment,
+    });
+  } catch (error) {
+    return next(createError(500, error.message));
+  }
+};
--- src/api/v1/Posts/PostsRouter.js
+++ src/api/v1/Posts/PostsRouter.js
@@ -11,7 +11,7 @@ const createError = require("http-errors");
 // --------------------------
 // Section: Custom Middlewares
 // --------------------------
-const { createPost } = require("./PostsMiddleware");
+const { createPost, getComment } = require("./PostsMiddleware");
 
 // --------------------------
 // Section: Posts Routers
@@ -24,6 +24,7 @@ router.route("/").get((req, res) => {
   });
 });
 router.route("/create").post(createPost);
+router.route("/comment/:commentParam").get(getComment);
 
 // --------------------------
 // Section: Books Error Handlers
